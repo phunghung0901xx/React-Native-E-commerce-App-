a84a29f8bb75eddf8d57d199f539a3b340040b32
phunghung <phunghung0901xx@gmail.com>
2023-07-21 11:40:58 +0700

    update

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import { useCallback } from 'react';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    regular: require("./assets/fonts/Poppins-Regular.ttf"),
+    light: require("./assets/fonts/Poppins-Light.ttf"),
+    bold: require("./assets/fonts/Poppins-Bold.ttf"),
+    medium: require("./assets/fonts/Poppins-Medium.ttf"),
+    extrabold: require("./assets/fonts/Poppins-ExtraBold.ttf"),
+    semibold: require("./assets/fonts/Poppins-SemiBold.ttf"),
+
+  })
+
+  const onLayoutRootView = useCallback(async() => {
+    if(fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+    if(!fontsLoaded) {
+      return null
+    }
+  },[fontsLoaded])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textStyle}>Open up App.js todsfsdf start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +38,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    fontFamily : "regular",
+    fontSize: 20,
+  }
 });
---
+++ navigation/BottomTabNavigation.jsx
@@ -0,0 +1,54 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Home from '../screens/Home'
+import Search from '../screens/Search'
+import Propfile from '../screens/Propfile'
+import { Ionicon } from "@expo/vector-icons"
+import { COLORS } from "../constants/index"
+
+const Tab = createBottomTabNavigator()
+
+const screenOptions = {
+    tabBarShowLabel: false,
+    tabBarHideOnKeyboard: true,
+    headerShown: false,
+    tabBarStyle: {
+        position: "absolute",
+        bottom: 0,
+        right: 0,
+        left: 0,
+        elevation: 0,
+        hegiht: 70
+    }
+}
+
+const BottomTabNavigation = () => {
+    return (
+        <Tab.Navigator screenOptions={screenOptions}>
+            <Tab.Screen name="Home" component={Home} options={
+                {
+                    tabBarIcon: ({ focused }) => {
+                        return <Ionicon name={focused ? "home" : "home-outline"} size={24} color={focused ? COLORS.primary : COLORS.gray2} />
+                    }
+                }
+            } />
+            <Tab.Screen name="Search" component={Search} options={
+                {
+                    tabBarIcon: ({ focused }) => {
+                        return <Ionicon name={"search-sharp"} size={24} color={focused ? COLORS.primary : COLORS.gray2} />
+                    }
+                }
+            } />
+           <Tab.Screen name="Propfile" component={Propfile} options={
+                {
+                    tabBarIcon: ({ focused }) => {
+                        return <Ionicon name={"search-sharp"} size={24} color={focused ? COLORS.primary : COLORS.gray2} />
+                    }
+                }
+            } />
+        </Tab.Navigator>
+    )
+}
+
+export default BottomTabNavigation
